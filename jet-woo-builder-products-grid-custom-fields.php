4a1522f217f56ad45224e18799ffba748b803901
Alexandr Rudy <aoikomete@gmail.com>
2021-07-23 11:41:42 +0300

    SYS: init

---
+++ jet-woo-builder-products-grid-custom-fields.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: JetWooBuilder - Products Grid Custom Fields
+ * Plugin URI:
+ * Description:
+ * Version:     1.0.0
+ * Author:      Crocoblock
+ * Author URI:  https://crocoblock.com/
+ * Text Domain: jet-woo-builder
+ * License:     GPL-3.0+
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ * Domain Path: /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die();
+}
+
+class Jet_Woo_Builder_Products_Grid_Custom_Fields {
+
+	public function __construct() {
+
+	}
+
+}
+
+new Jet_Woo_Builder_Products_Grid_Custom_Fields();
